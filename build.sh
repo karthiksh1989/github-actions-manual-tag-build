5b45664919eca381de0116db1a99df5a86b728b4
vensder <vensder@gmail.com>
2020-10-23 17:23:32 +1300

    fixed build

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-version=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
+# version=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
+
+version=$(git describe --tags --exact-match 2> /dev/null \
+  || git symbolic-ref -q --short HEAD \
+  || git rev-parse --short HEAD)
 
 echo "$version"
 export VERSION=$version
