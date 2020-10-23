240c21ad4005a950278896e5616a66820bdf3eef
vensder <vensder@gmail.com>
2020-10-23 17:27:09 +1300

    fixed build

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ version=$(git describe --tags --exact-match 2> /dev/null \
   || git symbolic-ref -q --short HEAD \
   || git rev-parse --short HEAD)
 
-echo "$version"
 export VERSION=$version
 echo "$VERSION"
